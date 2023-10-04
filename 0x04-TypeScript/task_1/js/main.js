79c4d21cdf1e8999bd5451b5de6c48c44294d2be
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2023-10-05 00:28:59 +0300

    files

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,31 @@
+"use strict";
+exports.__esModule = true;
+exports.printTeacher = void 0;
+var printTeacher = function (firstName, lastName) {
+  return firstName[0] + ". " + lastName;
+};
+exports.printTeacher = printTeacher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
